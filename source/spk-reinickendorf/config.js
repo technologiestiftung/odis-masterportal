5003bd4e2f1bc709815f1f4df3b2df2883960585
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2021-05-27 17:14:03 +0200

    add new content and styles

--- source/spk-reinickendorf/config.js
+++ source/spk-reinickendorf/config.js
@@ -2,23 +2,21 @@
 
 const Config = {
     wfsImgPath: "../spk-reinickendorf/ressources/img/",
-    namedProjections: [
-        ["EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
-    ],
+    namedProjections: [[
+        "EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+        ]],
     footer: {
-        urls: [
-            {
-                "bezeichnung": "Kartographie und Gestaltung: ",
-                "url": "https://www.berlin.de/ba-reinickendorf/politik-und-verwaltung/aemter/stadtentwicklungsamt/vermessung/",
-                "alias": "Bezirksamt Reinickendorf von Berlin, Fachbereich Vermessung",
-                "alias_mobil": "BA RDF, Verm"
-            }
-        ],
+        urls: [{
+            "bezeichnung": "Kartographie und Gestaltung: ",
+            "url": "https://www.berlin.de/ba-reinickendorf/politik-und-verwaltung/aemter/stadtentwicklungsamt/vermessung/",
+            "alias": "Bezirksamt Reinickendorf von Berlin, Fachbereich Vermessung",
+            "alias_mobil": "BA RDF, Verm"
+            }],
         showVersion: true
-    },
+        },
     quickHelp: {
         imgPath: "./ressources/img/"
-    },
+        },
     layerConf: "../spk-reinickendorf/ressources/services-internet.json",
     restConf: "../spk-reinickendorf/ressources/rest-services-internet.json",
     styleConf: "../spk-reinickendorf/ressources/style_v3.json",
@@ -26,8 +24,8 @@ const Config = {
     mouseHover: {
         numFeaturesToShow: 2,
         infoText: "(weitere Objekte. Bitte zoomen.)"
-    },
+        },
     useVectorStyleBeta: true
-};
+    };
 
 /* eslint-enable no-unused-vars */
